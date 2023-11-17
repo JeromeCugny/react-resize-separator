db5f528cd90c3506c730fb471cdf76a4327bfa50
Jerome Cugny <jerome.cugny@mphasis.com>
2023-11-17 17:02:28 -0600

    feat: example App

---
+++ examples/simple-app/src/index.tsx
@@ -0,0 +1,49 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import "bootstrap/dist/js/bootstrap.bundle"
+import "bootstrap/scss/bootstrap.scss"
+import "react-resize-separator/scss"
+import * as ResizeSeparator from "react-resize-separator"
+
+function Table() {
+  
+  return (
+    <React.Fragment>
+      <h1>Table cols</h1>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>col&nbsp;1</th>
+            <th>
+              <ResizeSeparator.HorizontalResizeHandle id="table-head-sep-1" className="resize-separator-vertical-absolute" />
+              col&nbsp;2
+            </th>
+            <th id="table-col-3">
+              <label>col&nbsp;3</label>
+              <ResizeSeparator.HorizontalResizeHandle id="table-head-sep-1" className="resize-separator-vertical-absolute" attachedElementId="table-col-3"/>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>value&nbsp;1</td>
+            <td>value&nbsp;2</td>
+            <td>value&nbsp;3</td>
+          </tr>
+        </tbody>
+      </table>
+    </React.Fragment>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Table />
+    </div>
+  )
+}
+
+const container = document.getElementById("root")
+const root = createRoot(container!)
+root.render(<App />)
---
+++ examples/simple-app/webpack.config.cjs
@@ -0,0 +1,48 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+const stylesHandler = MiniCssExtractPlugin.loader
+
+const config = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: "./src/index.html"}),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+        options: {
+          transpileOnly: true,
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: ["/node_modules/"],
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: ["/node_modules/"],
+        use: [stylesHandler, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".css", ".scss"],
+  },
+}
+
+module.exports = () => {
+  return config
+}
